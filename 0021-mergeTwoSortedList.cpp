4fce3b3bc23565fd1c94ef392200d484ebe25ee4
Chenglong Wei <longprowei@gmail.com>
2019-04-30 21:56:34 +1000

    add recursive method, faster than 100%

--- 0021-mergeTwoSortedList.cpp
+++ 0021-mergeTwoSortedList.cpp
@@ -15,6 +15,19 @@ struct ListNode {
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == NULL) return l2;
+        if (l2 == NULL) return l1;
+
+        if (l1->val <= l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    };
+
+    ListNode* mergeTwoLists2(ListNode* l1, ListNode* l2) {
         if (l1 == NULL && l2 == NULL) {
             return NULL;
         } else if (l1 == NULL) {
