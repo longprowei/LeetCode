237b7f3c39860a0307b649ef5afce4862f1b98fa
Chenglong Wei <longprowei@gmail.com>
2022-04-22 11:53:30 +1000

    fix the #7 python solution

--- python/0007-reverseInteger.py
+++ python/0007-reverseInteger.py
@@ -4,24 +4,22 @@ class Solution:
         currNum = x if x > 0 else -x
         maxInt32 = 2 ** 31 - 1
         while currNum != 0:
-            ret = ret * 10 + currNum % 10
-            currNum //= 10
-
-            if ret < maxInt32:
-                continue
-
             if ret > maxInt32 // 10:
                 ret = 0
                 break
 
-            if x > 0:
-                if currNum % 10 > maxInt32 % 10:
-                    ret = 0
-                    break
-            else:
-                if currNum % 10 > maxInt32 % 10 + 1:
-                    ret = 0
-                    break
+            if ret == maxInt32 // 10:
+                if x > 0:
+                    if currNum % 10 > maxInt32 % 10:
+                        ret = 0
+                        break
+                else:
+                    if currNum % 10 > maxInt32 % 10 + 1:
+                        ret = 0
+                        break
+            
+            ret = ret * 10 + currNum % 10
+            currNum //= 10
 
         if x < 0:
             ret = -ret
@@ -35,3 +33,5 @@ assert s.reverse(-123) == -321
 assert s.reverse(120) == 21
 assert s.reverse(99999999999) == 0
 assert s.reverse(-999999999999) == 0
+assert s.reverse(1534236496) == 0
+assert s.reverse(-2147483412) == -2143847412
