060b77ffc6aa7112ca963673d91313e2f354311b
Chenglong Wei <longprowei@gmail.com>
2022-12-15 11:31:40 +1100

    finish #5 using cpp

--- cpp/0005-longestPalindrome.cpp
+++ cpp/0005-longestPalindrome.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
@@ -19,23 +20,49 @@ public:
 
             while (j >= 0 && k < s.length() && s[j] == s[k]) {
                 currLength += 2;
-                j--;
-                k++;
-
+                
                 if (currLength > maxLength) {
                     maxLength = currLength;
                     palindrome = s.substr(j, maxLength);
                 }
+
+                j--;
+                k++;
             }
         }
 
         for (int i = 1; i < s.length(); i++) {
             int j = i - 1;
             int k = i + 1;
+            int currLength = 1;
 
             while (j >= 0 && k < s.length() && s[j] == s[k]) {
+                currLength += 2;
 
+                if (currLength > maxLength) {
+                    maxLength = currLength;
+                    palindrome = s.substr(j, maxLength);
+                }
+
+                j--;
+                k++;
             }
         }
+
+        return palindrome.length() > 1 ? palindrome : s.substr(0, 1);
     }
 };
+
+int main() {
+    Solution s;
+    assert(s.longestPalindrome("a") == "a");
+    assert(s.longestPalindrome("abccbad") == "abccba");
+    assert(s.longestPalindrome("dabccba") == "abccba");
+    assert(s.longestPalindrome("dabccbabdbf") == "abccba");
+    assert(s.longestPalindrome("babad") == "bab");
+    assert(s.longestPalindrome("cbbd") == "bb");
+    assert(s.longestPalindrome("aaaaa") == "aaaaa");
+    assert(s.longestPalindrome("aacabdkacaa") == "aca");
+    assert(s.longestPalindrome("ac") == "a");
+    return 0;
+}
