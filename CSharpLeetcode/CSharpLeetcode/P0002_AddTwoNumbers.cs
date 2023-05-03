40788fb1a9300e1984f9ea8ad4b52bd26e775dc9
Chenglong Wei <longprowei@gmail.com>
2023-05-03 20:15:11 +1000

    finish problem #2 in C#

---
+++ CSharpLeetcode/CSharpLeetcode/P0002_AddTwoNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace CSharpLeetcode
+{
+
+    public class ListNode {
+        public int val;
+        public ListNode? next;
+        public ListNode(int val = 0, ListNode? next = null) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class P0002_AddTwoNumbers
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode dummyHead = new();
+            ListNode? curr = dummyHead;
+            ListNode p = l1, q = l2;
+            int carry = 0;
+            while (p != null || q != null)
+            {
+                int x = p != null ? p.val : 0;
+                int y = q != null ? q.val : 0;
+                curr.next = new ListNode();
+                curr = curr.next;
+                curr.val = (x + y + carry) % 10;
+                carry = (x + y + carry) / 10;
+
+                if (p != null) p = p.next;
+                if (q != null) q = q.next;
+            }
+
+            if (carry == 1)
+            {
+                curr.next = new ListNode();
+                curr = curr.next;
+                curr.val = carry;
+            }
+
+            return dummyHead.next;
+        }
+    }
+}
+
---
+++ CSharpLeetcode/CSharpLeetcodeTest/P0002_AddTwoNumbersTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using CSharpLeetcode;
+
+namespace CSharpLeetcodeTest
+{
+    public class P0002_AddTwoNumbersTest
+    {
+        [Fact]
+        public void AddTwoZero()
+        {
+            ListNode l1 = new();
+            ListNode l2 = new();
+            ListNode expected = new();
+
+            P0002_AddTwoNumbers solution = new();
+            ListNode actual = solution.AddTwoNumbers(l1, l2);
+
+            Assert.NotNull(actual);
+            Assert.Equal(expected.val, actual.val);
+            Assert.Equal(expected.next, actual.next);
+        }
+
+        [Fact]
+        public void AddTwoSingleNumber_WithoutCarry()
+        {
+            ListNode l1 = new(2);
+            ListNode l2 = new(3);
+            ListNode expected = new(5);
+
+            P0002_AddTwoNumbers solution = new();
+            ListNode actual = solution.AddTwoNumbers(l1, l2);
+
+            Assert.NotNull(actual);
+            Assert.Equal(expected.val, actual.val);
+            Assert.Equal(expected.next, actual.next);
+        }
+
+        [Fact]
+        public void AddTwoSingleNumber_WithCarry()
+        {
+            ListNode l1 = new(6);
+            ListNode l2 = new(7);
+            ListNode expected = new(3, new(1));
+
+            P0002_AddTwoNumbers solution = new();
+            ListNode actual = solution.AddTwoNumbers(l1, l2);
+
+            Assert.NotNull(actual);
+            Assert.Equal(expected.val, actual.val);
+            Assert.NotNull(actual.next);
+            Assert.Equal(expected.next.val, actual.next.val);
+        }
+
+        [Fact]
+        public void Add1And999()
+        {
+            ListNode l1 = new(1);
+            ListNode l2 = new(9, new(9, new(9)));
+            ListNode expected = new(0, new(0, new(0, new(1))));
+
+            P0002_AddTwoNumbers solution = new();
+            ListNode actual = solution.AddTwoNumbers(l1, l2);
+
+            Assert.NotNull(actual);
+            while(actual != null && expected != null)
+            {
+                Assert.Equal(expected.val, actual.val);
+                expected = expected.next;
+                actual = actual.next;
+            }
+            Assert.Null(actual);
+            Assert.Null(expected);
+        }
+
+        [Fact]
+        public void Add342And456()
+        {
+            ListNode l1 = new(2, new(4, new(3)));
+            ListNode l2 = new(5, new(6, new(4)));
+            ListNode expected = new(7, new(0, new(8)));
+
+            P0002_AddTwoNumbers solution = new();
+            ListNode actual = solution.AddTwoNumbers(l1, l2);
+
+            Assert.NotNull(actual);
+            while (actual != null && expected != null)
+            {
+                Assert.Equal(expected.val, actual.val);
+                expected = expected.next;
+                actual = actual.next;
+            }
+            Assert.Null(actual);
+            Assert.Null(expected);
+        }
+    }
+}
+
