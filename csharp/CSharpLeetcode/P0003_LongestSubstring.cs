f8c92e676e2a12d435a21aee474d6075199c1829
Chenglong Wei <longprowei@gmail.com>
2023-05-10 21:35:20 +1000

    finish #3 problem using C#

---
+++ csharp/CSharpLeetcode/P0003_LongestSubstring.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace CSharpLeetcode
+{
+    public class P0003_LongestSubstring
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int maxLen = 0;
+            Dictionary<char, int> charDict = new();
+
+            for (int i = 0, j = 0; j < s.Length; j++)
+            {
+                if (charDict.ContainsKey(s[j]))
+                {
+                    i = Math.Max(i, charDict[s[j]]);
+                }
+
+                maxLen = Math.Max(maxLen, j - i + 1);
+                charDict[s[j]] = j + 1;
+            }
+
+            return maxLen;
+        }
+    }
+}
+
---
+++ csharp/CSharpLeetcodeTest/P0003_LongestSubstringTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using CSharpLeetcode;
+
+namespace CSharpLeetcodeTest
+{
+    public class P0003_LongestSubstringTest
+    {
+        [Fact]
+        public void EmptyString()
+        {
+            string s = string.Empty;
+            int expected = 0;
+
+            P0003_LongestSubstring solution = new();
+            int actual = solution.LengthOfLongestSubstring(s);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void OneLetterString()
+        {
+            string s = "a";
+            int expected = 1;
+
+            P0003_LongestSubstring solution = new();
+            int actual = solution.LengthOfLongestSubstring(s);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void StringAbc()
+        {
+            string s = "abc";
+            int expected = 3;
+
+            P0003_LongestSubstring solution = new();
+            int actual = solution.LengthOfLongestSubstring(s);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void StringSameLetters()
+        {
+            string s = "aaaaaaa";
+            int expected = 1;
+
+            P0003_LongestSubstring solution = new();
+            int actual = solution.LengthOfLongestSubstring(s);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void StringAbcabcbb()
+        {
+            string s = "abcabcbb";
+            int expected = 3;
+
+            P0003_LongestSubstring solution = new();
+            int actual = solution.LengthOfLongestSubstring(s);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void StringPwwkew()
+        {
+            string s = "pwwkew";
+            int expected = 3;
+
+            P0003_LongestSubstring solution = new();
+            int actual = solution.LengthOfLongestSubstring(s);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void StringBbtablud()
+        {
+            string s = "bbtablud";
+            int expected = 6;
+
+            P0003_LongestSubstring solution = new();
+            int actual = solution.LengthOfLongestSubstring(s);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
+
